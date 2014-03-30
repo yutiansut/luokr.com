cef92d75da58ac0a5f83d7e53a058d2c9edb067a
Alvan <alvan.law@gmail.com>
2014-03-30 18:23:48 +0800

    do not use StaticFileHandler to serve file requests

--- www.luokr.com/V0.3/app/svr.py
+++ www.luokr.com/V0.3/app/svr.py
@@ -13,10 +13,6 @@ from tornado.options import define, options
 from app.url import url
 from app.etc import etc
 
-etc.setdefault('template_path', os.path.join(TOP_DIR, 'app', 'views'))
-etc.setdefault('static_path', os.path.join(TOP_DIR, 'www', 'assets'))
-etc.setdefault('static_url_prefix', '/assets/')
-
 app = tornado.web.Application(handlers = url, **etc)
 
 define("port", default=8001, help="run on the given port", type=int)
