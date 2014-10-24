c145caa140dd7aad1e8beb8c17496a918aa2233e
Alvan <alvan.law@gmail.com>
2014-10-24 18:16:49 +0800

    refine implements of score.py

--- www.luokr.com/app/ctrls/score.py
+++ www.luokr.com/app/ctrls/score.py
@@ -4,15 +4,15 @@ from basic import BasicCtrl
 
 class ScoreCtrl(BasicCtrl):
     def post(self):
-        post = self.model('posts').get_by_pid(self.dbase('posts'), self.input('p'))
-        if not post:
-            self.flash(0, {'msg': '文章不存在'})
+        pid = self.input('p', None)
+        if not pid:
+            self.flash(0)
             return
 
         con_posts = self.dbase('posts')
         cur_posts = con_posts.cursor()
 
-        cur_posts.execute('update posts set post_rank = post_rank + 1, post_plus = post_plus + 1 where post_id = ?', (post['post_id'],))
+        cur_posts.execute('update posts set post_rank = post_rank + 1, post_plus = post_plus + 1 where post_id = ? limit 1', (pid,))
 
         con_posts.commit()
         cur_posts.close()
