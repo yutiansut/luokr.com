9e4860e98e1ce533f41a86eef5c57eb65d271afe
Alvan <alvan.law@gmail.com>
2015-05-21 16:27:58 +0800

    add setup scripts

---
+++ www.luokr.com/app/setup/datas.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+
+import os.path, sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+sys.path[0] = os.path.dirname(os.path.dirname(sys.path[0]))
+
+from app.etc import etc
+import sqlite3
+
+def main():
+    for name, path in etc['dbase'].items():
+        if not os.path.exists(path):
+            print 'Make dbase: %s => %s' % (name, path)
+
+            dbn = sqlite3.connect(path)
+            dbn.executescript(open(os.path.join(os.path.dirname(path), name + '.sql')).read())
+            dbn.close()
+        else:
+            print 'Find dbase: %s => %s' % (name, path)
+
+if __name__ == "__main__":
+    main()
+
