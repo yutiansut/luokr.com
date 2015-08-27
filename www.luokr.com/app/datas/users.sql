8c473496797a0980acae9d68881cd61f05f85fb1
Alvan <alvan.law@gmail.com>
2015-08-27 23:21:39 +0800

    update datas/users.sql

--- www.luokr.com/app/datas/users.sql
+++ www.luokr.com/app/datas/users.sql
@@ -1,28 +1,21 @@
 
 -- Table: users
-CREATE TABLE users ( 
-    user_id   INTEGER         PRIMARY KEY AUTOINCREMENT,
-    user_auid VARCHAR( 64 )   NOT NULL
-                              DEFAULT ( '' ),
-    user_name VARCHAR( 64 )   NOT NULL
-                              UNIQUE
-                              COLLATE 'NOCASE',
-    user_salt CHAR( 8 )       NOT NULL,
-    user_pswd CHAR( 32 )      NOT NULL,
-    user_perm INT( 10 )       NOT NULL
-                              DEFAULT ( 0 ),
-    user_mail VARCHAR( 64 )   NOT NULL
-                              UNIQUE
-                              COLLATE 'NOCASE',
-    user_sign VARCHAR( 128 )  NOT NULL
-                              DEFAULT ( '' ),
-    user_logo VARCHAR( 256 )  NOT NULL
-                              DEFAULT ( '' ),
-    user_meta TEXT            NOT NULL
-                              DEFAULT ( '' ),
-    user_ctms INT( 10 )       NOT NULL,
-    user_utms INT( 10 )       NOT NULL,
-    user_atms INT( 10 )       NOT NULL 
+CREATE TABLE "users" (
+	 "user_id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	 "user_auid" VARCHAR (64,0) NOT NULL DEFAULT (''),
+	 "user_name" VARCHAR (64,0) NOT NULL COLLATE NOCASE,
+	 "user_salt" CHAR (8,0) NOT NULL,
+	 "user_pswd" CHAR (32,0) NOT NULL,
+	 "user_perm" INT (10,0) NOT NULL DEFAULT (0),
+	 "user_mail" VARCHAR (128,0) NOT NULL COLLATE NOCASE,
+	 "user_sign" VARCHAR (256,0) NOT NULL DEFAULT (''),
+	 "user_logo" VARCHAR (512,0) NOT NULL DEFAULT (''),
+	 "user_meta" TEXT NOT NULL DEFAULT (''),
+	 "user_ctms" INT (10,0) NOT NULL,
+	 "user_utms" INT (10,0) NOT NULL,
+	 "user_atms" INT (10,0) NOT NULL,
+	UNIQUE ("user_name" ASC),
+	UNIQUE ("user_mail" ASC)
 );
 
-INSERT INTO [users] ([user_id], [user_name], [user_salt], [user_pswd], [user_perm], [user_mail], [user_sign], [user_logo], [user_meta], [user_ctms], [user_utms], [user_atms]) VALUES (1, 'admin', 'asdflkjh', 'b6ce17f5578131e2997ccfb99dcc3500', 2147483647, '', '', '', '', 1374486661, 1374786660, 1374765138);
+INSERT INTO [users] ([user_id], [user_name], [user_salt], [user_pswd], [user_perm], [user_mail], [user_ctms], [user_utms], [user_atms]) VALUES (1, 'admin', 'asdflkjh', 'b6ce17f5578131e2997ccfb99dcc3500', 2147483647, '', 1374486661, 1374786660, 1374765138);
