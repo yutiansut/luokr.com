0e796f6c4892321807e57cc4f506d101d3c9f4b6
Alvan <alvan.law@gmail.com>
2017-05-17 14:49:01 +0800

    remove alive wrapper from basic ctrl

--- app/ctrls/basic.py
+++ app/ctrls/basic.py
@@ -251,14 +251,3 @@ def login(method):
             return
         return method(self, *args, **kwargs)
     return wrapper
-
-def alive(method):
-    @login
-    @functools.wraps(method)
-    def wrapper(self, *args, **kwargs):
-        if self.model('admin').chk_user_is_live(self.current_user):
-            return method(self, *args, **kwargs)
-        else:
-            self.flash(0, {'sta': 403, 'url': self.get_login_url()})
-            return
-    return wrapper
--- app/ctrls/shell/__init__.py
+++ app/ctrls/shell/__init__.py
@@ -2,15 +2,19 @@
 
 import functools
 
-from app.ctrls.basic import alive, BasicCtrl
+from app.ctrls.basic import login, BasicCtrl
 
 class ShellCtrl(BasicCtrl):
     pass
 
 def shell(method):
-    @alive
+    @login
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        return method(self, *args, **kwargs)
+        if self.model('admin').chk_user_is_live(self.current_user):
+            return method(self, *args, **kwargs)
+        else:
+            self.flash(0, {'sta': 403, 'url': self.get_login_url()})
+            return
     return wrapper
 
